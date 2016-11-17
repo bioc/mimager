3b081623aa3de2414eb1ad3b6e8144013edb2ded
Aaron Wolen <aaron@wolen.com>
2016-11-17 13:55:51 -0500

    Simplify to_arry()

--- R/utils-arrays.r
+++ R/utils-arrays.r
@@ -5,32 +5,18 @@
 
 to_array <- function(x, nrow, ncol, coords = NULL, transpose = TRUE, index) {
   if (is.null(coords)) coords <- arrayInd(1:nrow(x), c(nrow, ncol))
+  stopifnot(nrow(coords) == nrow(x))
 
   dim.names <- list(seq_len(nrow), seq_len(ncol), colnames(x))
   out <- array(dim = c(nrow, ncol, ncol(x)), dimnames = dim.names)
 
-  # location of value in the array
-  coords.index <- xy2index(coords[, 1], coords[, 2], nrow, ncol)
-
-  # scale matrix coordinates to multi-dimensional array
-  ncells <- prod(nrow, ncol)
-  offset <- ncells * seq_len(ncol(x)) - ncells
-  offset <- rep(offset, each = length(coords.index))
-  coords.index <- rep(coords.index, ncol(x)) + offset
-
+  coords.index <-
+    cbind(
+      dim1 = coords$x,
+      dim2 = coords$y,
+      dim3 = rep(seq_len(ncol(x)), each = nrow(coords))
+    )
   out[coords.index] <- as.numeric(x)
   if (transpose) out <- aperm(out, perm = c(2, 1, 3))
   out
 }
-
-
-# convert x,y coordinates to linear index
-# byrow: Default is FALSE (column-major order)
-xy2index <- function(x, y, nrow, ncol, byrow = FALSE) {
-  x <- x - 1
-  y <- y - 1
-  switch(byrow + 1,
-    (nrow * y + x) + 1,
-    (ncol * x + y) + 1)
-}
-
