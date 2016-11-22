347b949fb4ad2fbc864b34058515bc3ce80b5149
Aaron Wolen <aaron@wolen.com>
2016-11-22 12:31:45 -0500

    Import oligo classes

--- R/mimager-package.r
+++ R/mimager-package.r
@@ -1,5 +1,7 @@
 #' @importClassesFrom affy AffyBatch
 #' @importClassesFrom affyPLM PLMset
+#' @importClassesFrom oligoClasses FeatureSet
+#' @importClassesFrom oligo oligoPLM
 #' @importFrom Biobase featureNames
 #' @importFrom grid unit
 #' @import gtable
