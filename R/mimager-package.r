4ae42ad7f5265855cdb0d325f2a61bc7e74bd43c
Aaron Wolen <aaron@wolen.com>
2016-12-07 08:26:45 -0500

    Improve package documentation

--- R/mimager-package.r
+++ R/mimager-package.r
@@ -1,3 +1,28 @@
+#' mimager: The Microarray Imager
+#'
+#' \pkg{mimager} simplifies the creation of microarray images (sometimes called
+#' "pseudo-images") for the purpose of identifying problematic regional
+#' aberrations. Notable features include support for many of Bioconductor's core
+#' microarray data structures, providng compatibility with a variety of common
+#' microarray platforms, and the ability to visualize multiple microarrays
+#' simultaneously.
+#'
+#' The following Bioconductor microarray data structures are currently
+#' supported:
+#'
+#' \itemize{
+#'   \item \code{\link[affy]{AffyBatch-class}} for Affymetrix GeneChip
+#'     probe level data
+#'    \item \code{\link[affyPLM]{PLMset-class}} for probe-level
+#'      linear models fitted to Affymetrix GeneChip probe level data
+#'    \item \code{\link[oligoClasses]{FeatureSet-class}} for storing
+#'      Expression/Exon/SNP data from a variety of oligonucleotide platforms
+#'    \item \code{\link[oligo]{oligoPLM-class}} for probe-level linear models
+#'      fitted to any of the \code{FeatureSet}-like classes
+#' }
+#'
+#' @docType package
+#' @name mimager
 #' @importClassesFrom affy AffyBatch
 #' @importClassesFrom affyPLM PLMset
 #' @importClassesFrom oligoClasses FeatureSet
