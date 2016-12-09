b2ad12297a158512d906fc5ed63dc53b76b28031
Aaron Wolen <aaron@wolen.com>
2016-12-09 13:56:44 -0500

    Simplify AffyBatch layout/orientation tests

---
+++ tests/testthat/test-affybatch-layout.r
@@ -0,0 +1,31 @@
+context("AffyBatch layout")
+
+if (requireNamespace("affydata", quietly = TRUE)) {
+
+  data(Dilution, package = "affydata")
+  obj <- Dilution[, 1:4]
+  ary <- marray(obj, transpose = FALSE, probes = "all")
+
+  test_that("array dimensions", {
+    expect_identical(dimnames(ary)[[3]], Biobase::sampleNames(obj))
+    expect_identical(dim(ary)[1:2], dim(obj))
+  })
+
+  test_that("array orientation", {
+    # verify that position of NAs in the generated array match the locations of
+    # positions missing from the index
+    indx <- mindex(obj, probes = "all")
+    mask <- Matrix::sparseMatrix(i = indx$x, j = indx$y, dims = dim(obj))
+    expect_equal(which(is.na(ary[,,1])), Matrix::which(!mask))
+  })
+
+  test_that("Single sample objects return an array", {
+    obj <- obj[, 1]
+    ary <- marray(obj)
+
+    expect_is(ary, "array")
+    expect_identical(dimnames(ary)[[3]], Biobase::sampleNames(obj))
+    expect_identical(nrow(ary), nrow(obj))
+    expect_identical(ncol(ary), ncol(obj))
+  })
+}
